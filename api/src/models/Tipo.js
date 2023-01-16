d13a4968ce1f117b4bb7e54e86b7afb3af61f870
Jhamil Fernandez <j-fernandez95@hotmail.com>
2023-01-16 17:19:07 -0500

    Finished BDD

---
+++ api/src/models/Tipo.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Tipo",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
+  );
+};
